b643882758d46a95ee27fb6f1c86d95c4ff3cb46
uitaek <uitaek@tigrison.com>
2018-08-22 13:02:41 +0900

    First Commit

--- app/src/main/java/kr/co/treegames/sudokur/MainActivity.java
+++ app/src/main/java/kr/co/treegames/sudokur/MainActivity.java
@@ -5,12 +5,23 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Created by Hwang on 2018-07-25.
  *
  * Description :
  */
 public class MainActivity extends AppCompatActivity {
+    public enum LoopResult {
+        NONE,
+        BREAK_OUTSIDE,
+        BREAK_INSIDE,
+        CONTINUE
+    }
+
+    private static final String TAG = "Sudokur";
+
     public static final int BOARD_ROW = 9;
     public static final int BOARD_COLUMN = 9;
 
@@ -33,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
     };
     private Data[][] board = new Data[BOARD_COLUMN][BOARD_ROW];
 
+    public interface OnLoopListener {
+        LoopResult onLoop(int col, int row);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,61 +57,73 @@ public class MainActivity extends AppCompatActivity {
         print();
 
         this.findViewById(R.id.btn_thinking).setOnClickListener((v) -> {
+            rollback();
             thinking();
         });
     }
+    public void rollback() {
+        temp = new int[][] {
+                { 0, 8, 0, 0, 0, 0, 2, 0, 0 },
+                { 0, 1, 2, 0, 8, 0, 0, 3, 4 },
+                { 0, 3, 6, 2, 5, 9, 0, 1, 0 },
+                { 0, 9, 0, 8, 0, 0, 0, 0, 5 },
+                { 0, 0, 0, 5, 3, 6, 0, 0, 0 },
+                { 1, 0, 0, 0, 0, 4, 0, 8, 0 },
+                { 0, 6, 0, 4, 9, 8, 3, 5, 0 },
+                { 3, 5, 0, 0, 6, 0, 1, 4, 0 },
+                { 0, 0, 4, 0, 0, 0, 0, 6, 0 }
+        };
+        initialize();
+        updateHint();
+    }
     public void initialize() {
-        for (int column = 0; column < temp.length; column++) {
-            for (int row = 0; row < temp[column].length; row++) {
-                Data data = new Data();
-                data.answer = temp[column][row];
-                if (data.answer != 0) {
-                    data.isFixed = true;
-                }
-                board[column][row] = data;
-
-                if (!board[column][row].isFixed) {
-                    board[column][row].hint = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-                }
+        loopBoard((column, row) -> {
+            Data data = new Data();
+            data.answer = temp[column][row];
+            if (data.answer != 0) {
+                data.isFixed = true;
             }
-        }
+            board[column][row] = data;
+
+            if (!board[column][row].isFixed) {
+                board[column][row].hint = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            }
+            return LoopResult.NONE;
+        });
     }
     public void thinking() {
         new Thread(() -> {
             while (true) {
-                if (algorithmOnlyOneInsideBoard()/* && algorithmOnlyOneInsideBlock()*/) break;
+                if (algorithmOnlyOneInsideBoard() && algorithmOnlyOneInsideBlock()) break;
                 if (checkSuccess()) break;
             }
-            Log.d("DEBUG", "STOP");
+            Log.d(TAG, "STOP");
             print();
         }).start();
     }
     public void updateHint() {
-        for (int column = 0; column < board.length; column++) {
-            for (int row = 0; row < board[column].length; row++) {
-                if (board[column][row].isFixed) {
-                    int number = board[column][row].answer;
-                    int columnStart = column - (column % 3);
-                    int rowStart = row - (row % 3);
+        loopBoard((column, row) -> {
+            if (board[column][row].isFixed) {
+                int number = board[column][row].answer;
+                loopBox(column, row, (subCol, subRow) -> {
+                    board[subCol][subRow].hint[number - 1] = 0;
+                    return LoopResult.NONE;
+                });
 
-                    for (int subCol = columnStart; subCol < columnStart + 3; subCol++) {
-                        for (int subRow = rowStart; subRow < rowStart + 3; subRow++) {
-                            board[subCol][subRow].hint[number - 1] = 0;
-                        }
-                    }
-                    for (int subRow = 0; subRow < BOARD_ROW; subRow++) {
-                        board[column][subRow].hint[number - 1] = 0;
-                    }
-                    for (int subCol = 0; subCol < BOARD_COLUMN; subCol++) {
-                        board[subCol][row].hint[number - 1] = 0;
-                    }
+                for (int subRow = 0; subRow < BOARD_ROW; subRow++) {
+                    board[column][subRow].hint[number - 1] = 0;
+                }
+                for (int subCol = 0; subCol < BOARD_COLUMN; subCol++) {
+                    board[subCol][row].hint[number - 1] = 0;
                 }
             }
-        }
+            return LoopResult.NONE;
+        });
     }
     public boolean algorithmOnlyOneInsideBoard() {
-        for (int column = 0; column < board.length; column++) {
-            for (int row = 0; row < board[column].length; row++) {
+        AtomicBoolean result = new AtomicBoolean(true);
+        loopBoard((column, row) -> {
+            if (!board[column][row].isFixed) {
                 int count = 0;
                 for (int index = 0; index < board[column][row].hint.length; index++) {
                     if (board[column][row].hint[index] == 0) {
@@ -108,64 +135,61 @@ public class MainActivity extends AppCompatActivity {
                         if (board[column][row].hint[index] > 0) {
                             board[column][row].answer = board[column][row].hint[index];
                             board[column][row].isFixed = true;
-                            board[column][row].hint = new int[]{ 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                            board[column][row].hint = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
                             updateHint();
-                            return false;
+                            result.set(false);
+                            return LoopResult.BREAK_OUTSIDE;
                         }
                     }
                 }
             }
-        }
-        return true;
+            return LoopResult.NONE;
+        });
+        return result.get();
     }
     public boolean algorithmOnlyOneInsideBlock() {
-        boolean result = true;
-        for (int column = 0; column < board.length; column++) {
-            for (int row = 0; row < board[column].length; row++) {
-                if (!board[column][row].isFixed) {
-                    for (int index = 0; index < board[column][row].hint.length; index++) {
-                        int number = board[column][row].hint[index];
-                        if (number != 0) {
-                            int columnStart = column - (column % 3);
-                            int rowStart = row - (row % 3);
-
-                            boolean isOnlyOne = true;
-                            out: for (int subCol = columnStart; subCol < columnStart + 3; subCol++) {
-                                for (int subRow = rowStart; subRow < rowStart + 3; subRow++) {
-                                    if (!board[subCol][subRow].isFixed && column != subCol && row != subRow) {
-                                        for (int subIndex = 0; subIndex < board[subCol][subRow].hint.length; subIndex++) {
-                                            if (number == board[subCol][subRow].hint[subIndex]) {
-                                                isOnlyOne = false;
-                                                break out;
-                                            }
-                                        }
+        AtomicBoolean result = new AtomicBoolean(true);
+        loopBoard((column, row) -> {
+            if (!board[column][row].isFixed) {
+                for (int index = 0; index < board[column][row].hint.length; index++) {
+                    int number = board[column][row].hint[index];
+                    if (number != 0) {
+                        AtomicBoolean isOnlyOne = new AtomicBoolean(true);
+                        loopBox(column, row, (subCol, subRow) ->  {
+                            if (!board[subCol][subRow].isFixed && !(column == subCol && row == subRow)) {
+                                for (int subIndex = 0; subIndex < board[subCol][subRow].hint.length; subIndex++) {
+                                    if (number == board[subCol][subRow].hint[subIndex]) {
+                                        isOnlyOne.set(false);
+                                        return LoopResult.BREAK_OUTSIDE;
                                     }
                                 }
                             }
-                            if (isOnlyOne) {
-                                board[column][row].answer = number;
-                                board[column][row].isFixed = true;
-                                updateHint();
-                                result = false;
-                                continue;
-                            }
-//                            isOnlyOne = true;
-//
-//                            for (int subRow = 0; subRow < BOARD_ROW; subRow++) {
-//                            }
-//
-//                            if (isOnlyOne) {
-//                                board[column][row].answer = number;
-//                                board[column][row].isFixed = true;
-//                                updateHint();
-//                                continue;
-//                            }
+                            return LoopResult.NONE;
+                        });
+                        if (isOnlyOne.get()) {
+                            board[column][row].answer = number;
+                            board[column][row].isFixed = true;
+                            updateHint();
+                            result.set(false);
+                            return LoopResult.CONTINUE;
                         }
+//                        isOnlyOne.set(false);
+//
+//                        for (int subRow = 0; subRow < BOARD_ROW; subRow++) {
+//                        }
+//
+//                        if (isOnlyOne.get()) {
+//                            board[column][row].answer = number;
+//                            board[column][row].isFixed = true;
+//                            updateHint();
+//                            return LoopResult.CONTINUE;
+//                        }
                     }
                 }
             }
-        }
-        return true;
+            return LoopResult.NONE;
+        });
+        return result.get();
     }
     public boolean checkSuccess() {
         for (int column = 0; column < board.length; column++) {
@@ -185,7 +209,42 @@ public class MainActivity extends AppCompatActivity {
             }
             line.append("\n");
         }
-        Log.d(this.getClass().getName(), "-");
-        Log.d(this.getClass().getName(), line.toString());
+        Log.d(TAG, "-");
+        Log.d(TAG, line.toString());
+    }
+    public void loopBoard(OnLoopListener listener) {
+        outside: for (int column = 0; column < board.length; column++) {
+            inside: for (int row = 0; row < board[column].length; row++) {
+                if (listener != null) {
+                    switch (listener.onLoop(column, row)) {
+                        case CONTINUE:
+                            continue;
+                        case BREAK_OUTSIDE:
+                            break outside;
+                        case BREAK_INSIDE:
+                            break inside;
+                    }
+                }
+            }
+        }
+    }
+    public void loopBox(int column, int row, OnLoopListener listener) {
+        int columnStart = column - (column % 3);
+        int rowStart = row - (row % 3);
+
+        outside: for (int subCol = columnStart; subCol < columnStart + 3; subCol++) {
+            inside: for (int subRow = rowStart; subRow < rowStart + 3; subRow++) {
+                if (listener != null) {
+                    switch (listener.onLoop(subCol, subRow)) {
+                        case CONTINUE:
+                            continue;
+                        case BREAK_OUTSIDE:
+                            break outside;
+                        case BREAK_INSIDE:
+                            break inside;
+                    }
+                }
+            }
+        }
     }
 }
